7f3de2c6ba4a5e9bfb2738ff0735d03497158ad9
hyunsoo24 <sunni303356@naver.com>
2024-02-19 17:22:31 +0900

    refactor: tablet상태도 필수로 설정

--- lib/responsive.dart
+++ lib/responsive.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 class Responsive extends StatelessWidget {
   final Widget mobile;
-  final Widget? tablet;
+  final Widget tablet;
   final Widget desktop;
 
   const Responsive({
     Key? key,
     required this.mobile,
-    this.tablet,
+    required this.tablet,
     required this.desktop,
   }) : super(key: key);
 
@@ -27,8 +27,8 @@ class Responsive extends StatelessWidget {
     final Size size = MediaQuery.of(context).size;
     if (size.width > 992) {
       return desktop;
-    } else if (size.width >= 576 && tablet != null) {
-      return tablet!;
+    } else if (size.width >= 576) {
+      return tablet;
     } else {
       return mobile;
     }
